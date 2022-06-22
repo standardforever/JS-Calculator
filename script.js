bd57eff90ac56f335c0bdbacdf4bee2eb55841be
standardforever <standard.forever123@gmail.com>
2022-06-22 17:42:55 -0400

    updated

--- script.js
+++ script.js
@@ -1,46 +1,56 @@
-let num1 = prompt('Enter Your First Number:');
-
-let num2= prompt('Enter Your second Number:');
-let _sign = prompt("Enter The Operator '*, +, -, /'");
 
 function _addition(num1, num2)
 {   
-	const sum = num1 + num2
-	alert("Your anwer is:  " + sum);
+	alert("Your answer is :" + (num1 + num2));
 }
 
 function _subtract(num1, num2)
 {
-	alert("Your anwer is:  " + (num1 - num2));
+	alert("Your answer is:  " + (num1 - num2));
 }
 
 function _divide(num1, num2)
 {
-	alert("Your anwer is:  " + num1 / num2);
+	alert("Your answer is:  " + num1 / num2);
 }
 
 function _multiply(num1, mum2)
 {
-	alert("Your anwer is:  " + num1 * num2)
+	alert("Your answer is:  " + num1 * num2)
 }
 
-if (_sign == '*')
+let num1 = prompt('Enter Your First Number:');
+let num2= prompt('Enter Your second Number:');
+let _sign = prompt("Enter The Operator '*, +, -, /'");
+
+if (isNaN(num1) || isNaN(num2))
 {
-	_multiply(num1, num2);
-}
-else if(_sign == '/')
-{
-	_divide(num1, num2);
-}
-else if(_sign == '+')
-{
-	_addition(num1, num2);
-}
-else if (_sign == '-')
-{
-	_subtract(num1, num2);
+	throw new Error("Alphabets Not allowed Numbers only")
 }
 
-else{
-	alert("Input is Wrong");
+else
+{
+	num1 = parseFloat(num1);
+	num2 = parseFloat(num2);
+
+	if (_sign === '*')
+	{
+		_multiply(num1, num2);
+	}
+	else if(_sign == '/')
+	{
+		_divide(num1, num2);
+	}
+	else if(_sign == '+')
+	{
+		_addition(num1, num2);
+	}
+	else if (_sign == '-')
+	{
+		_subtract(num1, num2);
+	}
+
+	else{
+		alert("Input is Wrong");
+	}
 }
